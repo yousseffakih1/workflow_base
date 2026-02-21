3c36b920588d4125b1cd92c40da3e6cae4e33547
Youssef ALLAFKIH <youssef.fakih@gmail.com>
2026-02-21 14:00:59 +0000

    --

--- workflow_base/__manifest__.py
+++ workflow_base/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Workflow Base",
+    'name': "Approval Workflow Engine - BPM Process Designer",
 
-    'summary': "Visual workflow builder with drag & drop designer - Create approval workflows, stages & routes",
+    'summary': "Approval workflow, BPM, state machine, process automation, visual designer, stages & transitions",
 
     'description': """
 Workflow Base - Visual Workflow Builder for Odoo 18
